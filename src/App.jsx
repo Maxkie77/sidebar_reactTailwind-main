304c29058279d338db096953ab4a75bcb3c7574a
Maxkie77 <kriangkrai@intermedisoft.com>
2024-04-11 17:35:08 +0700

    modified:   src/App.jsx 	new file:   src/Dashboard.jsx 	new file:   src/NewBooking.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import NewBooking from "./NewBooking";
+
 const App = () => {
   const [open, setOpen] = useState(true);
   const Menus = [
@@ -17,54 +20,63 @@ const App = () => {
   ];
 
   return (
-    <div className="flex">
-      <div
-        className={` ${
-          open ? "w-72" : "w-20 "
-        } bg-gray-800 h-screen p-5  pt-8 relative duration-300`}
-      >
-        <img
-          src="./src/assets/control.png"
-          className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
-           border-2 rounded-full  ${!open && "rotate-180"}`}
-          onClick={() => setOpen(!open)}
-        />
-        <div className="flex gap-x-4 items-center">
+    <Router>
+      <div className="flex">
+        <div
+          className={` ${
+            open ? "w-72" : "w-20 "
+          } bg-gradient-to-b from-gray-600 to-gray-800 h-screen p-5  pt-8 relative duration-300`}
+        >
           <img
-            src="./src/assets/icons8-kappa-35.png"
-            className={`cursor-pointer duration-500 ${
-              open && "rotate-[360deg]"
-            }`}
+            src="./src/assets/control.png"
+            className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
+             border-2 rounded-full  ${!open && "rotate-180"}`}
+            onClick={() => setOpen(!open)}
           />
-          <h1
-            className={`text-white origin-left font-medium text-xl duration-200 ${
-              !open && "scale-0"
-            }`}
-          >
-            Office Admin
-          </h1>
-        </div>
-        <ul className="pt-6">
-          {Menus.map((Menu, index) => (
-            <li
-              key={index}
-              className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
-              ${Menu.gap ? "mt-8" : "mt-2"} ${
-                index === 0 && "bg-light-white"
-              } `}
+          <div className="flex gap-x-4 items-center">
+            <img
+              src="./src/assets/icons8-kappa-35.png"
+              className={`cursor-pointer duration-500 ${
+                open && "rotate-[360deg]"
+              }`}
+            />
+            <h1
+              className={`text-white origin-left font-medium text-xl duration-200 ${
+                !open && "scale-0"
+              }`}
             >
-              <img src={`./src/assets/${Menu.src}.png`} />
-              <span className={`${!open && "hidden"} origin-left duration-200`}>
-                {Menu.title}
-              </span>
-            </li>
-          ))}
-        </ul>
+              Office Admin
+            </h1>
+          </div>
+          <ul className="pt-6">
+            {Menus.map((Menu, index) => (
+              <li
+                key={index}
+                className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
+                ${Menu.gap ? "mt-8" : "mt-2"} ${
+                  index === 0 && "bg-light-white"
+                } `}
+              >
+                <img src={`./src/assets/${Menu.src}.png`} />
+                <span className={`${!open && "hidden"} origin-left duration-200`}>
+                  <Link to={Menu.title === "Dashboard" ? "/Dashboard" 
+                          : Menu.title === "New Booking" ? "/newbooking" 
+                          : `/${Menu.title.toLowerCase()}`} className="text-gray-300 hover:text-white">
+                    {Menu.title}
+                  </Link>
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="h-screen flex-1 p-7">
+          <Routes>
+            <Route path="/Dashboard" element={<Dashboard />} />
+            <Route path="/newbooking" element={<NewBooking />} />
+          </Routes>
+        </div>
       </div>
-      <div className="h-screen flex-1 p-7">
-        <h1 className="text-2xl font-semibold ">Home Page</h1>
-      </div>
-    </div>
+    </Router>
   );
 };
 export default App;
---
+++ src/NewBooking.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NewBooking = () => {
+  return (
+    <div>
+      <h1>NewBooking</h1>
+      {/*dashboard*/}
+    </div>
+  );
+};
+
+export default NewBooking;
